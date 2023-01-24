8c708e9f67cefc9ddd88550c1ad9cad1a13ff22b
benaissa-saida <benaissasaida28@gmail.com>
2023-01-24 19:14:04 +0100

    new function for number format

--- src/utils/textFormatter.js
+++ src/utils/textFormatter.js
@@ -7,3 +7,14 @@ export const capitalize = (s) => {
   if (typeof s !== "string") return "";
   return s.charAt(0).toUpperCase() + s.slice(1);
 };
+
+/**
+ * Format a number by grouping it to 3 digits after the decimal
+ * @param {number}
+ * @return {string}
+ */
+export const numberFormater = (number) => {
+  return new Intl.NumberFormat("en-IN", { maximumSignificantDigits: 3 }).format(
+    number
+  );
+};
