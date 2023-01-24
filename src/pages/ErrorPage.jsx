619835545d2eecb556c38cfc7c3ff573d3d367e8
benaissa-saida <benaissasaida28@gmail.com>
2023-01-24 19:17:07 +0100

    new error page structure

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,21 +1,21 @@
 import Layout from "../components/Layout";
 import { useRouteError } from "react-router-dom";
-// import "../styles/error.css";
+import "../styles/error.css";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 
 function ErrorPage() {
   const error = useRouteError();
   useEffect(() => {
-    document.title = `SportSee | ${error.statusText}`;
+    document.title = `SportSee | Error has occurred`;
   }, [error]);
   return (
     <Layout>
-      <div id="ks-error-page">
-        <h1 className="ks-status-error">{error.status}</h1>
-        <h2 className="ks-message-error">Oups! {error.message}</h2>
-        <div className="ks-return">
-          <Link to="/" className="ks-return-link">
+      <div id="Ss-error-page">
+        <h1 className="Ss-status-error">{error.status ? error.status : 400}</h1>
+        <h2 className="Ss-message-error">Oups! Une erreur est survenue</h2>
+        <div className="Ss-return">
+          <Link to="/" className="Ss-return-link">
             Retourner sur la page d’accueil
           </Link>
         </div>
